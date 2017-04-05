ff0ded2fbc87d217772b23fe87245e7f94aca55d
Florian Herrmann <florian.herrmann@m2m-systems.de>
2017-04-05 16:36:15 +0200

    Sensor Integration
    First test with all sensor integrated

--- src/kk-mgh-plantsensor.ino
+++ src/kk-mgh-plantsensor.ino
@@ -24,15 +24,42 @@
 
 #include "BluefruitConfig.h"
 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <Wire.h>
+
+#include <Adafruit_TSL2561_U.h>
+
+#define CHIRP_RESET 6
+
+#define DHTPIN 5
+#define DHTTYPE           DHT11
+
 #define FACTORYRESET_ENABLE         1
 #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 #define MODE_LED_BEHAVIOUR          "MODE"
 
 Adafruit_BluefruitLE_SPI ble( BLUEFRUIT_SPI_CS , BLUEFRUIT_SPI_IRQ , BLUEFRUIT_SPI_RST );
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
 void error( const __FlashStringHelper*err ) {
 	Serial.println( err );
 	while( 1 );
+}
+
+unsigned int readI2CRegister16bit(int addr, int reg) {
+	Wire.beginTransmission(addr);
+	Wire.write(reg);
+	Wire.endTransmission();
+	delay(1100);
+	Wire.requestFrom(addr, 2);
+	unsigned int t = Wire.read() << 8;
+	t = t | Wire.read();
+	return t;
 }
 
 void setup(void) {
@@ -40,6 +67,58 @@ void setup(void) {
 	while (!Serial);
 	delay(500);
 	Serial.begin(115200);
+
+	Serial.print( F("Initialising I2C ->" ) );
+	Wire.begin();
+	Serial.println( F("OK") );
+
+	Serial.print( F("Initialising CHIRP ->" ) );
+	pinMode( CHIRP_RESET , OUTPUT );
+	digitalWrite( CHIRP_RESET , LOW );
+	delay( 100 );
+	digitalWrite( CHIRP_RESET , HIGH );
+	delay( 500 );
+	readI2CRegister16bit( 0x20 , 0 );
+	Serial.println( F("OK") );
+
+	Serial.print( F( "Moisture Level -> " ) );
+	uint32_t chirpValue = readI2CRegister16bit( 0x20 , 0 );
+	Serial.println( chirpValue );
+
+	Serial.print( F("Initialising DHT11 ->" ) );
+	dht.begin();
+	Serial.println( F("OK") );
+
+	sensors_event_t event;
+  	dht.temperature().getEvent(&event);
+
+	if( isnan( event.temperature ) ) {
+    	Serial.println("Error reading temperature!");
+  	} else {
+    	Serial.print("Temperature: ");
+		Serial.print(event.temperature);
+    	Serial.println(" *C");
+  	}
+
+	dht.humidity().getEvent(&event);
+  	if( isnan( event.relative_humidity ) ) {
+    	Serial.println( "Error reading humidity!" );
+  	} else {
+    	Serial.print("Humidity: ");
+    	Serial.print(event.relative_humidity);
+    	Serial.println("%");
+  	}
+
+	Serial.print( F( "Initialising TSL2561 -> " ) );
+	tsl.begin();
+	tsl.enableAutoRange(true);
+    tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);
+	Serial.println( F("OK") );
+
+	Serial.print( F( "Light Level -> " ) );
+	tsl.getEvent( &event );
+	Serial.print( event.light );
+	Serial.println(" Lux");
 
 	Serial.print(F("Initialising the Bluefruit LE module -> "));
 
@@ -64,8 +143,6 @@ void setup(void) {
 	Serial.println( F( "----------------" ) );
 	Serial.println( F( "Bluefruit info" ) );
 	ble.info();
-
-
 
 	while( !ble.isConnected() ) {
 			delay(500);
@@ -97,7 +174,28 @@ void loop(void) {
 		Serial.println( receivedCmd );
 
 		if( receivedCmd.equals( "C:GetData" ) ) {
-			String answer = "A:123/12.3/12/12345";
+
+			sensors_event_t event;
+
+			dht.temperature().getEvent( &event );
+			int temperature = event.temperature;
+
+			dht.humidity().getEvent( &event );
+			int humidity = event.relative_humidity;
+
+			uint32_t moisture = readI2CRegister16bit( 0x20 , 0 );
+
+			tsl.getEvent( &event );
+			int light = event.light;
+
+			String answer = "A:";
+			answer += moisture;
+			answer += "/";
+			answer += temperature;
+			answer += "/";
+			answer += humidity;
+			answer += "/";
+			answer += light;
 
 			//Serial.print( "Answer Length -> " );
 			//Serial.println( answer.length() + 1 );
